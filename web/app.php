25e37b2de0231e063b36ccab2b86c211eae52c32
Xavier L <xlembo@eliberty.fr>
2015-09-27 21:44:38 +0200

    fix branch2 name

--- web/app.php
+++ web/app.php
@@ -38,7 +38,7 @@ $result = $conn->fetchAll('SELECT * FROM tags LIMIT 10');
         <pre>
         I am HOST <?php print_r( $_SERVER['SERVER_ADDR']); ?>
         </pre>
-		<h4>BRANCH 1 here are tags</h4>
+		<h4>BRANCH 2 here are tags</h4>
 		<ul>
 			<?php foreach ($result as $row): ?>
 				<li><?php echo $row['slug']?></li>
